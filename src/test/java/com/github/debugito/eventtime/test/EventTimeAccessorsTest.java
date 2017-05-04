029f8dc76e138b017b18d537f975f3f63c71aebc
Toshio Ito <debug.ito@gmail.com>
2017-05-04 16:50:40 +0900

    add test skeletons.

---
+++ src/test/java/com/github/debugito/eventtime/test/EventTimeAccessorsTest.java
@@ -0,0 +1,9 @@
+package com.github.debugito.eventtime.test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import com.github.debugito.eventtime.EventTime;
+
+public class EventTimeAccessorsTest {
+    // TODO
+}
