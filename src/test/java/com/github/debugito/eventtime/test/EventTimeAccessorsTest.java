70517c9c8b711715c1f07f6efbffa503c04f56b4
Toshio Ito <debug.ito@gmail.com>
2017-05-04 17:43:05 +0900

    implement EventTimeAccessorsTest

--- src/test/java/com/github/debugito/eventtime/test/EventTimeAccessorsTest.java
+++ src/test/java/com/github/debugito/eventtime/test/EventTimeAccessorsTest.java
@@ -1,9 +1,35 @@
 package com.github.debugito.eventtime.test;
 import static org.junit.Assert.assertEquals;
+import static com.github.debugito.eventtime.test.Util.et;
 import org.junit.Test;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
 
 import com.github.debugito.eventtime.EventTime;
 
 public class EventTimeAccessorsTest {
-    // TODO
+    public static ZonedDateTime zdt(int y, int m, int d, int H, int M, int S, int ns, String zone) {
+        return ZonedDateTime.of(y, m, d, H, M, S, ns, ZoneId.of(zone));
+    }
+
+    public static ZonedDateTime zdt(int y, int m, int d, int H, int M, int S, int ns) {
+        return ZonedDateTime.of(y, m, d, H, M, S, ns, ZoneId.systemDefault());
+    }
+
+
+    public static void test(EventTime etime, ZonedDateTime exp_zdt, boolean exp_is_time_explicit,
+                            boolean exp_is_time_zone_explicit) {
+        assertEquals(etime.getZonedDateTime(), exp_zdt);
+        assertEquals(etime.getZonedDateTime().getZone(), exp_zdt.getZone());
+        assertEquals(etime.isTimeExplicit(), exp_is_time_explicit);
+        assertEquals(etime.isTimeZoneExplicit(), exp_is_time_zone_explicit);
+    }
+
+    @Test
+    public void testAccessors() {
+        test(et(2017,5,2,"+09:00"), zdt(2017,5,2,0,0,0,0,"+09:00"), false, true);
+        test(et(2016,12,23), zdt(2016,12,23,0,0,0,0), false, false);
+        test(et(2017,3,6,21,44,10,567,"+09:00"), zdt(2017,3,6,21,44,10,567,"+09:00"), true, true);
+        test(et(2017,3,6,21,44,10,567), zdt(2017,3,6,21,44,10,567), true, false);
+    }
 }
--- src/test/java/com/github/debugito/eventtime/test/Util.java
+++ src/test/java/com/github/debugito/eventtime/test/Util.java
@@ -8,6 +8,10 @@ public class Util {
         return new EventTime(year, month, day_of_month, ZoneId.of(zone_id));
     }
 
+    public static EventTime et(int year, int month, int day_of_month) {
+        return new EventTime(year, month, day_of_month);
+    }
+
     public static EventTime et(int year, int month, int day_of_month,
                                int hour, int minute, int second, int nanosecond,
                                String zone_id) {
@@ -15,5 +19,8 @@ public class Util {
                              ZoneId.of(zone_id));
     }
 
-
+    public static EventTime et(int year, int month, int day_of_month,
+                               int hour, int minute, int second, int nanosecond) {
+        return new EventTime(year, month, day_of_month, hour, minute, second, nanosecond);
+    }
 }
