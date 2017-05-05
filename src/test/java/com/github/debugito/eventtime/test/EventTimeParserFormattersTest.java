5f8a3c87da88d8618ec5d25bfba0a85e132a7a2e
Toshio Ito <debug.ito@gmail.com>
2017-05-05 17:53:45 +0900

    add testFormatter

--- src/test/java/com/github/debugito/eventtime/test/EventTimeParserFormattersTest.java
+++ src/test/java/com/github/debugito/eventtime/test/EventTimeParserFormattersTest.java
@@ -20,6 +20,8 @@ public class EventTimeParserFormattersTest {
         testP("2017-04-20", et(2017,4,20));
         testP("2016-10-31Z", et(2016,10,31,"Z"));
         testP("2016-09-11+09:00", et(2016,9,11,"+09:00"));
+        testP("2016-11-22+09:00[Asia/Tokyo]", et(2016,11,22,"Asia/Tokyo"));
+        testP("2016-09-10[Asia/Tokyo]", et(2016,9,10,"Asia/Tokyo"));
         testP("2016-11-24T19:04:00", et(2016,11,24,19,4,0,0));
         testP("2016-11-22T04:00:32.903", et(2016,11,22,4,0,32,903000000));
         testP("2017-01-10T00:52:10+09:00", et(2017,1,10,0,52,10,0,"+09:00"));
@@ -27,5 +29,21 @@ public class EventTimeParserFormattersTest {
         testP("2007-12-03T10:15:30+01:00[Europe/Paris]", et(2007,12,3,10,15,30,0,"Europe/Paris"));
         testP("2015-06-11T00:14:09.045+09:00[Asia/Tokyo]", et(2015,6,11,0,14,9,45000000,"Asia/Tokyo"));
         testP("2016-10-11T09:40:12[Asia/Tokyo]", et(2016,10,11,9,40,12,0,"Asia/Tokyo"));
+    }
+
+    public static void testF(EventTime etime, String exp_output) {
+        assertEquals(etime.toString(), exp_output);
+    }
+
+    @Test
+    public void testFormatter() {
+        testF(et(2016,10,31, "Z"), "2016-10-31Z");
+        testF(et(2016,9,11,"+09:00"), "2016-09-11+09:00");
+        testF(et(2016,11,22,"Asia/Tokyo"), "2016-11-22[Asia/Tokyo]");
+        testF(et(2017,1,10,0,52,10,0,"+09:00"), "2017-01-10T00:52:10+09:00");
+        testF(et(2016,7,31,22,10,8,405000000,"Z"), "2016-07-31T22:10:08.405Z");
+        testF(et(2007,12,3,10,15,30,0,"Europe/Paris"), "2007-12-03T10:15:30[Europe/Paris]");
+        testF(et(2015,6,11,0,14,9,45000000,"Asia/Tokyo"), "2015-06-11T00:14:09.045[Asia/Tokyo]");
+        testF(et(2016,10,11,9,40,12,0,"Asia/Tokyo"), "2016-10-11T09:40:12[Asia/Tokyo]");
     }
 }
